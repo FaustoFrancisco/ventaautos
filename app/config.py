287c49c2ea97495cae6e4689d1d0fd0697101913
FaustoFrancisco <FaustoFrancisco@users.noreply.github.com>
2023-09-02 16:13:59 -0600

    Update config.py
    se agrega el leguaje palabra reservada postgresql

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://bdpostgres_fp2y_user:VUUHKHvJha3FqEYs6pwRvRY0Vy2V70ia@dpg-cjpok6u1208c73dg767g-a/bdpostgres_fp2y"
+    SQLALCHEMY_DATABASE_URI = "postgresql://bdpostgres_fp2y_user:VUUHKHvJha3FqEYs6pwRvRY0Vy2V70ia@dpg-cjpok6u1208c73dg767g-a/bdpostgres_fp2y"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     
