13cab364361063c67c12c11f943def7303cfec19
FAUSTO FRANCISCO SOLIS <fsolis@qualitas.com.mx>
2023-09-09 12:03:35 -0600

    SIN CORES

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+#from flask_cors import CORS
 
 #Crear app de Flask
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 #Configurando la aplicacion
 app.config.from_object(Config)
 
--- app/models.py
+++ app/models.py
@@ -7,4 +7,5 @@ class Autos(db.Model):
     imagen = db.Column(db.String())
     precio = db.Column(db.Numeric(7,2))
     estado = db.Column(db.Boolean)
+    puertas = db.Column(db.Integer)
 
